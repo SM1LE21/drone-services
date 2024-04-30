9e1da31f428fd998270a63d570e1e3ba07fd2d63
SM1LE21 <lasknut@gmail.com>
2024-04-30 19:45:36 +0200

    changing template

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import GlobalStyles from 'styles/GlobalStyles';
-import { css } from "styled-components/macro"; //eslint-disable-line
-
-import AgencyLandingPage from "demos/AgencyLandingPage.js";
-
-export default AgencyLandingPage;
