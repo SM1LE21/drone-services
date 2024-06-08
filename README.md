1ca9fc64acee7884637bb97715537c1928f201c8
SM1LE21 <lasknut@gmail.com>
2024-06-08 21:23:54 +0200

    SEO optimization

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ To run the application:
 ``` 
 npm run dev
 ````
+
+To Build the application:
+``` 
+npm run build
+````
+
+To get the changes to the production branch, checkout production after building, then delete everything but dist/ node_modules/ and .gitignore. Next copy the content of dist/ to the root and delete the dist/.
+
+
+SEO: to regenerate sitemap (only needed if new sections or pages are added) run (for this node is needed):
+``` 
+npm run sitemap
+````
---
+++ src/generate-sitemap.js
@@ -0,0 +1,27 @@
+// file used to generate sitemap.xml
+// 'npm run sitemap' to generate sitemap
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const sitemap = new SitemapStream({ hostname: 'https://www.swiftdrones.net' });
+
+const links = [
+  { url: '/#services', changefreq: 'daily', priority: 1.0 },
+  { url: '/#about', changefreq: 'daily', priority: 0.8 },
+  { url: '/faq', changefreq: 'weekly', priority: 0.7 },
+];
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+(async () => {
+  const writeStream = createWriteStream(path.join(__dirname, '../public/sitemap.xml'));
+  sitemap.pipe(writeStream);
+
+  links.forEach(link => sitemap.write(link));
+  sitemap.end();
+
+  await streamToPromise(sitemap).then(sm => sm.toString());
+})();
